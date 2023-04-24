f9c4e0da2b7a2e4e4fa421b0ec422dd7ab322fa1
Shahar Halevy2 <ron1591590@gmail.com>
2023-04-24 17:59:09 +0300

    find_freq and mental_attention updates

--- scripts/mental_attention_state_detection_to_spikes.py
+++ scripts/mental_attention_state_detection_to_spikes.py
@@ -82,8 +82,9 @@ def get_EEG_data(data_root, filename):
     return dat
 
 
-data_root = '../datasets/EEG_data_for_Mental_Attention_State_Detection/EEG_Data/'
-files = os.listdir(data_root)
+#data_root = '../datasets/EEG_data_for_Mental_Attention_State_Detection/EEG_Data/'
+#data_root = '../datasets/IMU_data/vector1/'
+#files = os.listdir(data_root)
 
 
 def get_trial_data(trial):
@@ -91,14 +92,14 @@ def get_trial_data(trial):
     return dat
 
 
-example_data = get_trial_data(1)
-channel_names = example_data.columns
+#example_data = get_trial_data(1)
+#channel_names = example_data.columns
 
 # 5 subjects, each subject did 7 trials. except the 5'th subject that did 6 trials.
 #  The first 2 trials were used to get familiar with the process.
 
-subject_map = {i: np.arange(3, 8) + 7 * (i - 1) for i in range(1, 5)}
-subject_map[5] = np.arange(3, 7) + 7 * 4
+#subject_map = {i: np.arange(3, 8) + 7 * (i - 1) for i in range(1, 5)}
+#subject_map[5] = np.arange(3, 7) + 7 * 4
 
 
 # number of samples in the original and resampled signals
@@ -120,78 +121,81 @@ def generate_spikes(resonator, data_resampled, spikes_output_path=None):
         )
 
 
-def create_datasets(time_of_sample_s, overlap, trials, output_path):
-    trials_folder = '../datasets/EEG_data_for_Mental_Attention_State_Detection/EEG_spikes'
-    signal_fs = 8415
-    spikes_in_sample = signal_fs * time_of_sample_s
-    step = int(spikes_in_sample * (1 - overlap))
-    for trial in tqdm(trials):
-        # every sample of 1s is 8Kb of memory!
-        # try:
-        # for ch_name in os.listdir(f'{trials_folder}/{trial}'):
-        #     for clk_freq in os.listdir(f'{trials_folder}/{trial}/{ch}'):
-        #         for f0 in os.listdir(f'{trials_folder}/{trial}/{ch}/{clk_freq}'):
-        #             np.load(f'{trials_folder}/{trial}/{ch_name}/{clk_freq}/{f0}')['spikes'].astype(np.int8)
-        # except ValueError:
-        #     raise ValueError(f'Exception at {trials_folder}/{trial}/{ch_name}/{clk_freq}/{f0}')
-
-        spikes = np.array([
-            [
-                np.load(f'{trials_folder}/{trial}/{ch_name}/{clk_freq}/{f0}')['spikes'].astype(np.int8)
-                for clk_freq in os.listdir(f'{trials_folder}/{trial}/{ch}')
-                for f0 in os.listdir(f'{trials_folder}/{trial}/{ch}/{clk_freq}')
-            ]
-            for ch_name in os.listdir(f'{trials_folder}/{trial}')
-        ])
-        for sample_start_index in range(0, spikes.shape[-1] - spikes_in_sample, step):
-            sample_start_time = int(sample_start_index / signal_fs * 1000)
-            file_name = f'{trial}_{sample_start_time}'
-            np.savez_compressed(
-                file=f'{output_path}/{file_name}',
-                spikes=spikes[:, :, sample_start_time:sample_start_time + spikes_in_sample]
-            )
 
 
-clk_resonators = {
-    16830: ['0.657', '1.523', '2.120', '2.504', '3.490'],
-    88402: ['4.604', '5.180', '5.755', '6.791', '8.000'],
-    154705: ['8.058', '9.065', '10.072', '11.885', '14.000'],
-    331510: ['15.108', '17.266', '19.424', '21.583', '25.468'],
-    696172: ['36.259', '40.791', '45.324', '53.482', '63.000']
-}
 
-fs = 128
-channels = ['AF3', 'F7', 'F3', 'FC5', 'T7', 'P7', 'O1', 'O2', 'P8', 'T8', 'FC6', 'F4', 'F8', 'AF4']
-ch_i = 1
-ch = channels[ch_i]
-subject_name = 4
-subject_trials = subject_map[subject_name]
+# def create_datasets(time_of_sample_s, overlap, trials, output_path):
+#     trials_folder = '../datasets/EEG_data_for_Mental_Attention_State_Detection/EEG_spikes'
+#     signal_fs = 8415
+#     spikes_in_sample = signal_fs * time_of_sample_s
+#     step = int(spikes_in_sample * (1 - overlap))
+#     for trial in tqdm(trials):
+#         # every sample of 1s is 8Kb of memory!
+#         # try:
+#         # for ch_name in os.listdir(f'{trials_folder}/{trial}'):
+#         #     for clk_freq in os.listdir(f'{trials_folder}/{trial}/{ch}'):
+#         #         for f0 in os.listdir(f'{trials_folder}/{trial}/{ch}/{clk_freq}'):
+#         #             np.load(f'{trials_folder}/{trial}/{ch_name}/{clk_freq}/{f0}')['spikes'].astype(np.int8)
+#         # except ValueError:
+#         #     raise ValueError(f'Exception at {trials_folder}/{trial}/{ch_name}/{clk_freq}/{f0}')
+#
+#         spikes = np.array([
+#             [
+#                 np.load(f'{trials_folder}/{trial}/{ch_name}/{clk_freq}/{f0}')['spikes'].astype(np.int8)
+#                 for clk_freq in os.listdir(f'{trials_folder}/{trial}/{ch}')
+#                 for f0 in os.listdir(f'{trials_folder}/{trial}/{ch}/{clk_freq}')
+#             ]
+#             for ch_name in os.listdir(f'{trials_folder}/{trial}')
+#         ])
+#         for sample_start_index in range(0, spikes.shape[-1] - spikes_in_sample, step):
+#             sample_start_time = int(sample_start_index / signal_fs * 1000)
+#             file_name = f'{trial}_{sample_start_time}'
+#             np.savez_compressed(
+#                 file=f'{output_path}/{file_name}',
+#                 spikes=spikes[:, :, sample_start_time:sample_start_time + spikes_in_sample]
+#             )
 
-trails = []
 
-n_channels = 14
-n_resonators = 5 * 5
-with tqdm(total=n_channels * len(trails) * n_resonators) as pbar:
-    for trial in trails:
-        print(trial)
-        data = get_trial_data(trial)
-        for ch_i, ch in enumerate(data.columns):
-            ch_data = data[ch].values
-            data_resampled = resample_signal(16830 / 2, fs, ch_data)
-            for clk_i, (clk_freq, list_of_f0) in enumerate(clk_resonators.items()):
-                spikes_folder = f'../datasets/EEG_data_for_Mental_Attention_State_Detection/EEG_spikes/{trial}/{ch}/{clk_freq}'
-                if not os.path.exists(spikes_folder):
-                    os.makedirs(spikes_folder)
-                for f_i, f0 in enumerate(list_of_f0):
-                    pbar.set_description(f'trial: {trial}, ch: {ch_i}/14 clk {clk_i}/5 f:{f_i}/5')
-                    pbar.update()
-                    spikes_file = f'{spikes_folder}/{f0}.npz'
-                    if Path(spikes_file).is_file():
-                        continue
-                    resonator = create_excitatory_inhibitory_resonator(
-                        freq0=f0,
-                        clk_freq=clk_freq)
-                    resonator.log_out_spikes(-1)
-                    generate_spikes(resonator, data_resampled, spikes_file)
+# clk_resonators = {
+#     16830: ['0.657', '1.523', '2.120', '2.504', '3.490'],
+#     88402: ['4.604', '5.180', '5.755', '6.791', '8.000'],
+#     154705: ['8.058', '9.065', '10.072', '11.885', '14.000'],
+#     331510: ['15.108', '17.266', '19.424', '21.583', '25.468'],
+#     696172: ['36.259', '40.791', '45.324', '53.482', '63.000']
+# }
 
-create_datasets(3, .5, range(31, 35), '../datasets/EEG_data_for_Mental_Attention_State_Detection/train_test_dataset')
+# fs = 128
+# channels = ['AF3', 'F7', 'F3', 'FC5', 'T7', 'P7', 'O1', 'O2', 'P8', 'T8', 'FC6', 'F4', 'F8', 'AF4']
+# ch_i = 1
+# ch = channels[ch_i]
+# subject_name = 4
+# subject_trials = subject_map[subject_name]
+#
+# trails = []
+#
+# n_channels = 14
+# n_resonators = 5 * 5
+# with tqdm(total=n_channels * len(trails) * n_resonators) as pbar:
+#     for trial in trails:
+#         print(trial)
+#         data = get_trial_data(trial)
+#         for ch_i, ch in enumerate(data.columns):
+#             ch_data = data[ch].values
+#             data_resampled = resample_signal(16830 / 2, fs, ch_data)
+#             for clk_i, (clk_freq, list_of_f0) in enumerate(clk_resonators.items()):
+#                 spikes_folder = f'../datasets/EEG_data_for_Mental_Attention_State_Detection/EEG_spikes/{trial}/{ch}/{clk_freq}'
+#                 if not os.path.exists(spikes_folder):
+#                     os.makedirs(spikes_folder)
+#                 for f_i, f0 in enumerate(list_of_f0):
+#                     pbar.set_description(f'trial: {trial}, ch: {ch_i}/14 clk {clk_i}/5 f:{f_i}/5')
+#                     pbar.update()
+#                     spikes_file = f'{spikes_folder}/{f0}.npz'
+#                     if Path(spikes_file).is_file():
+#                         continue
+#                     resonator = create_excitatory_inhibitory_resonator(
+#                         freq0=f0,
+#                         clk_freq=clk_freq)
+#                     resonator.log_out_spikes(-1)
+#                     generate_spikes(resonator, data_resampled, spikes_file)
+
+#create_datasets(3, .5, range(31, 35), '../datasets/EEG_data_for_Mental_Attention_State_Detection/train_test_dataset')
--- tests/find_freq.py
+++ tests/find_freq.py
@@ -1,89 +1,147 @@
 import matplotlib.pyplot as plt
+from scripts.mental_attention_state_detection_to_spikes import resample_signal , generate_spikes
 import pandas as pd
 import numpy as np
 from matplotlib import mlab
 from spicy import signal
 import os
+from tqdm import tqdm
+from pathlib import Path
 
+from snn.resonator import create_excitatory_inhibitory_resonator
 
-path = "C:\\Users\ozzil\Desktop\project\kaggle_data\\tdcsfog"
-first = True
-#N = 97077  # number of max elements
-#f="C:\\Users\ozzil\Desktop\project\kaggle_data\\00c4c9313d.parquet"
-i=0
+path = "C:\\Program Files (x86)\project\\tdcsfog"
+
+i = 0
 fig, ax = plt.subplots()
-mean = 0
-count = 0
-for filename in os.listdir(path):
-    count += 1
-    f = os.path.join(path,filename)
-    #print(f)
-    data = pd.read_csv(f,index_col=0)
-    #data = pd.read_parquet(f, engine='fastparquet')
 
-    #data = data['Torque'].astype(float).values
-    #print(data)
-    npArray=np.array(data)
-    sig1=npArray[:,0]
-    N = len(sig1)
-    mean += N
-    #sig2=npArray[:,1]
-    avg = sum(sig1)/N
-    sig1 = sig1 - avg
-    f, t, Sxx = signal.spectrogram(sig1, fs=128)
-    ax.pcolormesh(t, f, Sxx, shading='gouraud')
-    #sig3=npArray[:,2]
-    #while len(sig1) < 97077:
-        #np.append(sig1,0)
-        #np.append(sig2,0)
-        #np.append(sig3,0)
-    #if max < N:
-    #    max = N
+clk_freq=15360
+fs=128
+channels = ['AccV', 'AccML', 'AccAP']
+clk_resonators = {
+    15360: ['0.6', '1.0', '1.39', '1.64', '1.93']
 
-    #if first == True:
-        #first = False
-        #fft1_sum = np.fft.fft(sig1,N)
-        #fft2_sum = np.fft.fft(sig2, N)
-        #fft3_sum = np.fft.fft(sig3, N)
-        #fftfreq = np.fft.fftfreq(2359, 1 / 128)
-        #freq = np.linspace(0, 128, N)
-    #else:
-        #sig1_sum += sig1
-        #sig2_sum += sig2
-        #sig3_sum += sig3
-        #fft1_sum += np.fft.fft(sig1,2359)
-        #fft2_sum += np.fft.fft(sig2, N)
-        #fft3_sum += np.fft.fft(sig3, N)
+}
+n_trials=803
+n_channels = 3
+n_resonators =5
+with tqdm(total=n_channels * n_resonators * n_trials) as pbar:
+    for trial in os.listdir(path):
+        f = os.path.join(path, trial)
+        data = pd.read_csv(f, index_col=0)
+        npArray = np.array(data)
+        for ch_i, ch_n in enumerate(channels):
+            ch_data = npArray[:, ch_i]
+            data_resampled = resample_signal(clk_freq / 2, fs, ch_data)
+            for clk_i, (clk_freq, list_of_f0) in enumerate(clk_resonators.items()):
+                spikes_folder = f'../datasets/IMU_data/{trial}/{ch_n}/{clk_freq}'
+                if not os.path.exists(spikes_folder):
+                    os.makedirs(spikes_folder)
+                for f_i, f0 in enumerate(list_of_f0):
+                    pbar.set_description(f',trial: {trial}, ch: {ch_i}/3 clk {clk_i}/1 f:{f_i}/5')
+                    pbar.update()
+                    spikes_file = f'{spikes_folder}/{f0}.npz'
+                    if Path(spikes_file).is_file():
+                        continue
+                    resonator = create_excitatory_inhibitory_resonator(
+                        freq0=f0,
+                        clk_freq=clk_freq)
+                    resonator.log_out_spikes(-1)
+                    generate_spikes(resonator, data_resampled, spikes_file)
 
 
-#print(len(fft3_sum))
-#figure, axis = plt.subplots(1, 3)
 
-#plt.ylabel("Frequency")
-#plt.xlabel("Time")
-#plt.plot(fftfreq[100:-100],abs(fft1_sum[100:-100]))
-#plt.plot(fftfreq,fft2)
-#plt.plot(fftfreq,abs(fft1_sum))
-#plt.plot(np.fft.fftfreq(N, 1 / 128),abs(np.fft.fft(sig1)))
 
-print(mean/count)
-#ax = plt.axes(projection='3d')
-#spec, freqs, t = mlab.specgram(sig1, Fs=128)
-#X, Y, Z = t[None, :], freqs[:, None], spec
-#ax.plot_surface(X, Y, Z, cmap='viridis')
-#ax.set_xlabel('time (s)')
-#ax.set_ylabel('frequencies (Hz)')
-#ax.set_zlabel('amplitude (dB)')
-#ax.set_ylim(0, 15)
 
-#axis[0]
 
-#plt.colorbar()
-#axis[1].
-#plt.specgram(np.fft.ifft(fft1_sum), Fs=128)
-plt.ylim(top=15)
-#plt.xlim(right=100)
-plt.yticks(np.arange(0,15,1))
-plt.hlines(np.arange(0,15,1),xmin=0,xmax=750,colors='w',linewidth=0.2)
-#axis[2].specgram(fft3_sum, Fs=128)
-plt.show()
+
+
+
+
+
+
+
+
+# path = "C:\\Program Files (x86)\project\\tdcsfog"
+# first = True
+# #N = 97077  # number of max elements
+# #f="C:\\Users\ozzil\Desktop\project\kaggle_data\\00c4c9313d.parquet"
+# i=0
+# fig, ax = plt.subplots()
+# mean = 0
+# count = 0
+# for filename in os.listdir(path):
+#     count += 1
+#     f = os.path.join(path,filename)
+#     #print(f)
+#     data = pd.read_csv(f,index_col=0)
+#     #data = pd.read_parquet(f, engine='fastparquet')
+#
+#     #data = data['Torque'].astype(float).values
+#     #print(data)
+#     npArray=np.array(data)
+#     sig1=npArray[:,0]
+#     #print(sig1)
+#     N = len(sig1)
+#     mean += N
+#     #sig2=npArray[:,1]
+#     avg = sum(sig1)/N
+#     sig1 = sig1 - avg
+#     f, t, Sxx = signal.spectrogram(sig1, fs=128)
+#     ax.pcolormesh(t, f, Sxx, shading='gouraud')
+#
+#     #sig3=npArray[:,2]
+#     #while len(sig1) < 97077:
+#         #np.append(sig1,0)
+#         #np.append(sig2,0)
+#         #np.append(sig3,0)
+#     #if max < N:
+#     #    max = N
+#
+#     #if first == True:
+#         #first = False
+#         #fft1_sum = np.fft.fft(sig1,N)
+#         #fft2_sum = np.fft.fft(sig2, N)
+#         #fft3_sum = np.fft.fft(sig3, N)
+#         #fftfreq = np.fft.fftfreq(2359, 1 / 128)
+#         #freq = np.linspace(0, 128, N)
+#     #else:
+#         #sig1_sum += sig1
+#         #sig2_sum += sig2
+#         #sig3_sum += sig3
+#         #fft1_sum += np.fft.fft(sig1,2359)
+#         #fft2_sum += np.fft.fft(sig2, N)
+#         #fft3_sum += np.fft.fft(sig3, N)
+#
+#
+# #print(len(fft3_sum))
+# #figure, axis = plt.subplots(1, 3)
+#
+# #plt.ylabel("Frequency")
+# #plt.xlabel("Time")
+# #plt.plot(fftfreq[100:-100],abs(fft1_sum[100:-100]))
+# #plt.plot(fftfreq,fft2)
+# #plt.plot(fftfreq,abs(fft1_sum))
+# #plt.plot(np.fft.fftfreq(N, 1 / 128),abs(np.fft.fft(sig1)))
+#
+# print(mean/count)
+# #ax = plt.axes(projection='3d')
+# #spec, freqs, t = mlab.specgram(sig1, Fs=128)
+# #X, Y, Z = t[None, :], freqs[:, None], spec
+# #ax.plot_surface(X, Y, Z, cmap='viridis')
+# #ax.set_xlabel('time (s)')
+# #ax.set_ylabel('frequencies (Hz)')
+# #ax.set_zlabel('amplitude (dB)')
+# #ax.set_ylim(0, 15)
+#
+# #axis[0]
+#
+# #plt.colorbar()
+# #axis[1].
+# #plt.specgram(np.fft.ifft(fft1_sum), Fs=128)
+# plt.ylim(top=15)
+# #plt.xlim(right=100)
+# plt.yticks(np.arange(0,15,1))
+# plt.hlines(np.arange(0,15,1),xmin=0,xmax=750,colors='w',linewidth=0.2)
+#
+# plt.show()
